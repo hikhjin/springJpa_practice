e18c86cbc6316d2019d4a07a8c85cb28852cda8a
hikhjin <hikhjin@gmail.com>
2024-02-20 21:25:28 +0900

    section1

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,8 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -12,11 +14,28 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
-        Member member = new Member();
-        member.setId(1L);
-        member.setName("HelloA");
+        try {
+            //비영속
+            Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB");
+            //영속
+            em.persist(member);
 
-        em.persist(member);
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("Hello1");
+
+            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
 
         em.close();
         emf.close();
