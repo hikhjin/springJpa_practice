25631d8fa3b8eb976f7c08a7665f32a1a64dd4b3
hikhjin <hikhjin@gmail.com>
2024-02-21 14:27:06 +0900

    영속성 컨텍스트

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,20 +16,23 @@ public class JpaMain {
 
         try {
             //비영속
-            Member member = new Member();
-            member.setId(2L);
-            member.setName("HelloB");
+            Member member = new Member(3L, "member3");
+//            member.setId(3L);
+//            member.setName("HelloB");
             //영속
             em.persist(member);
+
+            em.flush();
 
 //            Member findMember = em.find(Member.class, 1L);
 //            findMember.setName("Hello1");
 
-            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
+//            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+//            for (Member member : result) {
+//                System.out.println("member.name = " + member.getName());
+//            }
 
+            System.out.println("-----------------");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
